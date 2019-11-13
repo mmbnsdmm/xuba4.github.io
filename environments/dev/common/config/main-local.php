4c023998a6b0529a1d5ffed6715b0f60952f1f18
wodrow-jyy-pc <wodrow451611cv@gmail.com>
2019-11-13 13:28:57 +0800

    init

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -8,6 +8,24 @@ return [
             'password' => "",
             'charset' => "utf8mb4",
         ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => false,    //这里一定要改成false，不然邮件不会发送
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.2980.com',
+                'username' => 'wodrow@2980.com',
+                'password' => '',
+            ],
+            'messageConfig'=>[
+                'charset'=>'UTF-8',
+                'from'=>['wodrow@2980.com'=>'wodrow@2980']
+            ],
+        ],
+        'apiTool' => [
+            'class' => \common\components\ApiTool::class,
+            'base_uri' => "http://initYii.bnsdmm.tk",
+        ],
     ],
     'aliases' => [
         '@bower' => "@vendor/bower",
@@ -19,5 +37,6 @@ return [
         'quene_yii_task_cache_key' => "quene_yii_task_cache_key",
         'yiichina-username' => "",
         'yiichina-password' => "",
+        'admin_role_ordinary_user_name' => "普通用户",
     ],
 ];
