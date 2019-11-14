5011ee6bddcd9afe181fc218b652ffa018b21fae
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-11-14 11:58:33 +0800

    update

--- environments/dev/api/config/main-local.php
+++ environments/dev/api/config/main-local.php
@@ -12,11 +12,11 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
+    /*$config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         'allowedIPs' => ['127.0.0.1', '::1', 'localhost', '192.168.*'],
-    ];
+    ];*/
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
