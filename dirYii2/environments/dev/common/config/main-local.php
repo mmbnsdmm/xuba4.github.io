3fef8595bf7df43c69bde11a12279d2b9dd1077c
wodrow-tigo120-tc <wodrow451611cv@gmail.com>
2020-01-27 17:27:28 +0800

    init

--- dirYii2/environments/dev/common/config/main-local.php
+++ dirYii2/environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => "yii\db\Connection",
-            'dsn' => "mysql:host=127.0.0.1;dbname=",
+            'dsn' => "mysql:host=127.0.0.1;port=3306;dbname=",
             'username' => "",
             'password' => "",
             'charset' => "utf8mb4",
