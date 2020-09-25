f099f2adb4a5307bc6df75b60a6ffc678cc6928f
eekoo-vm-ws2019 <wodrow451611cv@gmail.com>
2020-09-25 15:54:21 +0800

    add YII_BASE_URL

--- common/config/_urlManagers.php
+++ common/config/_urlManagers.php
@@ -9,16 +9,16 @@
 $urlManagers = [
     'urlManagerHome' => [
         'class' => \yii\web\UrlManager::class,
-        'baseUrl' => getenv("BASE_URL")."/home",
-        'hostInfo' => getenv("BASE_URL"),
+        'baseUrl' => YII_BASE_URL."/home",
+        'hostInfo' => YII_BASE_URL,
         'enablePrettyUrl' => true,
         'showScriptName' => false,
 //        'enableStrictParsing' => true,
     ],
     'urlManagerAdmin' => [
         'class' => \yii\web\UrlManager::class,
-        'baseUrl' => getenv("BASE_URL")."/admin",
-        'hostInfo' => getenv("BASE_URL"),
+        'baseUrl' => YII_BASE_URL."/admin",
+        'hostInfo' => YII_BASE_URL,
         'enablePrettyUrl' => true,
         'showScriptName' => false,
 //        'enableStrictParsing' => true,
@@ -28,8 +28,8 @@ $urlManagers = [
 $_apiConfig = \wodrow\yii\rest\Controller::getConfig();
 $urlManagers['urlManagerApi'] = $_apiConfig['components']['urlManager'];
 $urlManagers['urlManagerApi']['class'] = \yii\web\UrlManager::class;
-$urlManagers['urlManagerApi']['baseUrl'] = getenv("BASE_URL")."/api";
-$urlManagers['urlManagerApi']['hostInfo'] = getenv("BASE_URL");
+$urlManagers['urlManagerApi']['baseUrl'] = YII_BASE_URL."/api";
+$urlManagers['urlManagerApi']['hostInfo'] = YII_BASE_URL;
 //$urlManagers['urlManagerApi']['rules']['/'] = 'route/api';
 
 return $urlManagers;
