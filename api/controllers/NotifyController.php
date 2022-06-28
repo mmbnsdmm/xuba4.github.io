92306f5a8412e09f15126f515d4d433579b55914
wodrow-teclast360 <wodrow451611cv@gmail.com>
2022-06-28 14:50:06 +0800

    add alipay notify push

--- api/controllers/NotifyController.php
+++ api/controllers/NotifyController.php
@@ -14,6 +14,9 @@ use wodrow\yii\rest\Controller;
 
 class NotifyController extends Controller
 {
+    /**
+     * gogo支付notify
+     */
     public function actionGogozhifuNotify()
     {
         $this->onlyDataOut = true;
@@ -22,11 +25,25 @@ class NotifyController extends Controller
         return "success";
     }
 
+    /**
+     * gogo支付return
+     */
     public function actionGogozhifuReturn()
     {
         $this->onlyDataOut = true;
         Tools::yiiLog($_GET);
         Tools::yiiLog($_POST);
         return "success";
+    }
+
+    /**
+     * 我的个人支付宝通知推送
+     */
+    public function actionWoPersonAliPayNotify($eStr, $sign)
+    {
+        return $this->success("succcess", [
+            'eStr' => $eStr,
+            'sign' => $sign,
+        ]);
     }
 }
